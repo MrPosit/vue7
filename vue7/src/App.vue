2885060806bf2a89de9e8db09149cf274539a3af
Еркин Адилжан Асхатулы <ErkinAd@lcoast.itstep>
2024-03-17 16:03:50 +0500

    first commit

---
+++ vue7/src/App.vue
@@ -0,0 +1,36 @@
+<script>
+import axios from "axios";
+export default {
+  data: () => {
+    return {
+      info: null,
+      EURInfo: null,
+      USDInfo: null,
+      GBPInfo: null,
+    };
+  },
+  watch: {
+    info() {
+      this.EURInfo = this.info.EUR;
+      this.USDInfo = this.info.USD;
+      this.GBPInfo = this.info.GBP;
+      console.log(this.EURInfo);
+    },
+  },
+  mounted() {
+    axios
+      .get("https://api.coindesk.com/v1/bpi/currentprice.json")
+      .then((response) => response.data)
+      .then((res) => {
+        this.info = res.bpi;
+        console.log(this.info);
+      });
+  },
+};
+</script>
+
+<template>
+  <div class="card">{{ info }}</div>
+</template>
+
+<style scoped></style>
